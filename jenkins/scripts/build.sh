ee6d866456b0894207ae0519ffabc2efe42956b1
chengmboy <chengng_mboy@163.com>
2019-09-03 18:22:33 +0800

    update

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -5,13 +5,18 @@ docker login -u ${DOCKER_HUB_CREDS_USR} -p ${DOCKER_HUB_CREDS_PSW}
 
 jars_path=$(find . -name *.jar |grep -v source |grep -v remote |grep -v common)
 
+echo 1
+
 for jar_file in ${jars_path[*]}
 
 do
 
     image=chengmboy/$(echo $jar_file | awk -F "[/]" '{print $NF}'|cut -d '-'  -f 1-2)
 
-    docker build --build-arg JAR_FILE=$jar_file -t ${params.DOCKER_IMAGE}:${GIT_TAG} .
+    echo $image
+    echo ${GIT_TAG}
+
+    docker build --build-arg JAR_FILE=$jar_file -t $image:${GIT_TAG} .
 
     docker push $image:${GIT_TAG}
 
