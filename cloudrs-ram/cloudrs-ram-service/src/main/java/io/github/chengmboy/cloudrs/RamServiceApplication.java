cc033800add924650f41392f09c35211fcd35ea3
chengmboy <chengng_mboy@163.com>
2019-09-12 17:52:29 +0800

    增加swagger

--- cloudrs-ram/cloudrs-ram-service/src/main/java/io/github/chengmboy/cloudrs/RamServiceApplication.java
+++ cloudrs-ram/cloudrs-ram-service/src/main/java/io/github/chengmboy/cloudrs/RamServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableSwagger2
 public class RamServiceApplication {
 
 	public static void main(String[] args) {
--- cloudrs-ram/cloudrs-ram-service/src/main/java/io/github/chengmboy/cloudrs/ram/controller/UserController.java
+++ cloudrs-ram/cloudrs-ram-service/src/main/java/io/github/chengmboy/cloudrs/ram/controller/UserController.java
@@ -8,9 +8,12 @@ import io.github.chengmboy.cloudrs.ram.pojo.dto.UserDTO;
 import io.github.chengmboy.cloudrs.ram.pojo.dto.UserRegisterDTO;
 import io.github.chengmboy.cloudrs.ram.pojo.vo.UserVO;
 import io.github.chengmboy.cloudrs.ram.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +28,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 @RequiredArgsConstructor
 @Slf4j
+@Api
 public class UserController {
 
     private final UserService userService;
     @PostMapping
-    public Response<UserVO> register(@RequestBody UserRegisterDTO user) throws GeneralSecurityException {
+    public Response<UserVO> register(@Validated @ApiParam @RequestBody UserRegisterDTO user) throws GeneralSecurityException {
         log.info(JSONObject.toJSONString(user));
         UserDTO userDTO = userService.register(user);
         UserVO userVO = new UserVO();
