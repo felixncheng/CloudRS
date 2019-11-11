7bf4cc5d3528f7bc295f9ad301c1e7965e8968d4
chengmeinan <cheng_mboy163.com>
2019-11-11 14:11:04 +0800

    update

--- cloudrs-auth/src/main/java/io/github/chengmboy/cloudrs/auth/AuthServiceApplication.java
+++ cloudrs-auth/src/main/java/io/github/chengmboy/cloudrs/auth/AuthServiceApplication.java
@@ -15,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class AuthServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
