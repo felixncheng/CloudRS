3d069db537df0e33b04a0ae0961974c0cf5c9bed
chengmeinan <cheng_mboy163.com>
2019-11-11 15:19:54 +0800

    update

--- clear_images.sh
+++ clear_images.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while IFS='' read -r line; do docker rmi "$line"; done < <(docker images|grep -i hub.chengmboy.com:8003/cloudrs |awk '{print $1":"$2}')
+while IFS='' read -r line; do docker rmi "$line"; done < <(docker images|grep -i cloudrs |awk '{print $3}')
